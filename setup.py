c35f82466344e040b6d32e76a8be9d1f08e12e5d
Sietse Snel <s.t.snel@uu.nl>
2024-10-16 11:43:04 +0200

    Release version v1.0.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.9.1',
+    version='1.0.0',
 
     description='''CKAN extension for the CD2 catalog.''',
     long_description=long_description,
